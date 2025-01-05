a3d0b40170f6f93085cdbc19768f3caa338d0eb0
Parth Sojitra <ps24abq@herts.ac.uk>
2025-01-05 15:29:24 +0000

    Add files via upload
    Signed-off-by: Parth Sojitra <ps24abq@herts.ac.uk>

---
+++ Histogram.R
@@ -0,0 +1,47 @@
+library(ggplot2)
+library(dplyr)
+library(readr)
+
+url <- "https://raw.githubusercontent.com/rahul-1195/A250-DS-304/main/Auto_theft%20.csv"
+data <- read_csv(url)
+
+data$Auto_Theft_Recovered <- as.numeric(data$Auto_Theft_Recovered)
+stats <- data %>%
+  group_by(Area_Name) %>%
+  summarise(
+    mean_recovered = mean(Auto_Theft_Recovered, na.rm = TRUE),
+    sd_recovered = sd(Auto_Theft_Recovered, na.rm = TRUE)
+  )
+
+data <- merge(data, stats, by = "Area_Name")
+ggplot(data, aes(x = Auto_Theft_Recovered, fill = Area_Name, color = Area_Name)) +
+  geom_histogram(aes(y = ..density..), position = "identity", alpha = 0.4, binwidth = 5) +
+  stat_function(
+    fun = dnorm,
+    args = list(mean = mean(data$Auto_Theft_Recovered[data$Area_Name == unique(data$Area_Name)[1]], na.rm = TRUE),
+                sd = sd(data$Auto_Theft_Recovered[data$Area_Name == unique(data$Area_Name)[1]], na.rm = TRUE)),
+    color = scales::hue_pal()(3)[1], size = 1
+  ) +
+  stat_function(
+    fun = dnorm,
+    args = list(mean = mean(data$Auto_Theft_Recovered[data$Area_Name == unique(data$Area_Name)[2]], na.rm = TRUE),
+                sd = sd(data$Auto_Theft_Recovered[data$Area_Name == unique(data$Area_Name)[2]], na.rm = TRUE)),
+    color = scales::hue_pal()(3)[2], size = 1
+  ) +
+  stat_function(
+    fun = dnorm,
+    args = list(mean = mean(data$Auto_Theft_Recovered[data$Area_Name == unique(data$Area_Name)[3]], na.rm = TRUE),
+                sd = sd(data$Auto_Theft_Recovered[data$Area_Name == unique(data$Area_Name)[3]], na.rm = TRUE)),
+    color = scales::hue_pal()(3)[3], size = 1
+  ) +
+  labs(
+    title = "Histogram with Bell Curve Overlays",
+    x = "Auto Theft Recovered (Units)",
+    y = "Density"
+  ) +
+  theme_minimal() +
+  theme(
+    plot.title = element_text(size = 16, face = "bold"),
+    axis.title.x = element_text(size = 14),
+    axis.title.y = element_text(size = 14)
+  )
