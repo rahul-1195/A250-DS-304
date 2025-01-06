e1f329dc4509d35dd24dab1512a380acf99d2a10
Parth Sojitra <ps24abq@herts.ac.uk>
2025-01-06 17:31:53 +0000

    Add files via upload
    Signed-off-by: Parth Sojitra <ps24abq@herts.ac.uk>

--- Rplot.R
+++ Rplot.R
@@ -1,20 +1,14 @@
-library(ggplot2)
-library(readr)
-
-url <- "https://raw.githubusercontent.com/rahul-1195/A250-DS-304/main/Auto_theft%20.csv"
-auto_theft_data <- read_csv(url)
-auto_theft_data$Area_Name <- as.factor(auto_theft_data$Area_Name)
-auto_theft_data$Auto_Theft_Recovered <- as.numeric(auto_theft_data$Auto_Theft_Recovered)
-
-ggplot(auto_theft_data, aes(x = Area_Name, y = Auto_Theft_Recovered, fill = Area_Name)) +
+ggplot(filtered_data, aes(x = Auto_Theft_Recovered, y = Area_Name, fill = Area_Name)) +
   geom_boxplot(outlier.color = "red", outlier.shape = 16, alpha = 0.7) +
   labs(
     title = "Box Plot of Auto Theft Recovered by Area Name",
-    x = "Area Name",
-    y = "Auto Theft Recovered"
+    x = "Auto Theft Recovered (Units)",
+    y = "Area Name"
   ) +
   theme_minimal() +
   theme(
-    axis.text.x = element_text(angle = 45, hjust = 1),
+    plot.title = element_text(size = 16, face = "bold"),
+    axis.title.x = element_text(size = 14),
+    axis.title.y = element_text(size = 14),
     legend.position = "none"
   )
